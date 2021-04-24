866e4119d6b714b8e11076768cf3fb7a4afacb8e
armando-sys <patrickarmando87@gmail.com>
2021-04-24 14:22:58 +0700

    Update collect.js

--- pages/collect.js
+++ pages/collect.js
@@ -9,15 +9,18 @@ export default function test() {
   return (
     <>
       <OrganismNav />
+
       <center>
         <div className="row container mt-5">
           <div className="col text-left">
-            <AtomText value="Peduli Digital Market" size="36px" weight="bold" />
+            <AtomText value="Hamlin Cellyn Tas Ransel Wanita Japan Style Big Compartment" size="36px" weight="bold" />
             <AtomText
               value="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged"
               size="14px"
             />
+
             <div className="row mt-5">
+
               <div className="col-5">
                 <AtomButton
                   background="#0086CF"
@@ -28,6 +31,7 @@ export default function test() {
                   radius="10px"
                 />
               </div>
+
               <div className="col">
                 <AtomButton
                   background="#F0F0F0"
@@ -39,10 +43,12 @@ export default function test() {
               </div>
             </div>
           </div>
+
           <div className="col">
             <img src="/beranda/img_beranda.svg" />
           </div>
         </div>
+
         <div
           className="mt-5 pt-5"
           style={{ width: "100%", height: "512px", background: "#F9F9F9" }}
