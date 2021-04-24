5e107f4c7d7ad6872c9ee6065f0202d81c2b9d35
armando-sys <patrickarmando87@gmail.com>
2021-04-24 20:59:53 +0700

    Create billing.js

---
+++ pages/billing.js
@@ -0,0 +1,15 @@
+import AtomText from "../components/atoms/text";
+import AtomButton from "../components/atoms/button";
+import MoleculeCollect from "../components/molecules/collect";
+import OrganismNav from "../components/organisms/nav";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Navbar, Nav, Form, FormControl } from "react-bootstrap";
+
+export default function billing (){
+    return (
+        <>
+        <OrganismNav />
+        
+        </>
+    )
+}
