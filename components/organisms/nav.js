2f2b670f501765935d8cff60db7d1bdd721df46c
armando-sys <patrickarmando87@gmail.com>
2021-04-24 13:00:51 +0700

    upd

---
+++ components/organisms/nav.js
@@ -0,0 +1,28 @@
+import { Button, Navbar, Nav, Form, FormControl } from "react-bootstrap";
+export default function nav() {
+  return (<>
+    <Navbar style={{ background: "#0086CF" }}>
+      <Navbar.Brand href="#home">
+        <img src="/beranda/logo.svg" />
+      </Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link className="text-light" href="#home">
+          Tren
+        </Nav.Link>
+        <Nav.Link className="text-light" href="#features">
+          Collection
+        </Nav.Link>
+        <Nav.Link className="text-light" href="#features">
+          Explore
+        </Nav.Link>
+        <Nav.Link className="text-light" href="#pricing">
+          About Us
+        </Nav.Link>
+      </Nav>
+      <Form inline>
+        <img className="mr-4" src="/beranda/logo_cart.svg" />
+        <img className="mr-4" src="/beranda/logo_user.svg" />
+      </Form>
+    </Navbar>
+    </>)
+}
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,48 @@
 import AtomText from "../components/atoms/text";
 import AtomButton from "../components/atoms/button";
+import OrganismNav from "../components/organisms/nav";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {Button, Navbar, Nav, Form, FormControl} from "react-bootstrap";
+import { Button, Navbar, Nav, Form, FormControl } from "react-bootstrap";
 
 export default function test() {
-    return (
-      <>
-        <Navbar style={{ background: "#0086CF" }}>
-          <Navbar.Brand href="#home">
-            <img src="/beranda/logo.svg" />
-          </Navbar.Brand>
-          <Nav className="mr-auto">
-            <Nav.Link className="text-light" href="#home">
-              Tren
-            </Nav.Link>
-            <Nav.Link className="text-light" href="#features">
-              Collection
-            </Nav.Link>
-            <Nav.Link className="text-light" href="#features">
-              Explore
-            </Nav.Link>
-            <Nav.Link className="text-light" href="#pricing">
-              About Us
-            </Nav.Link>
-          </Nav>
-          <Form inline>
-            <img className="mr-4" src="/beranda/logo_cart.svg" />
-            <img className="mr-4" src="/beranda/logo_user.svg" />
-          </Form>
-        </Navbar>
-      </>
-    );
-  }
+  return (
+    <>
+      <OrganismNav />
+      <center>
+        <div className="row container mt-5">
+          <div className="col text-left">
+            <AtomText value="Peduli Digital Market" size="36px" weight="bold" />
+            <AtomText
+              value="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged"
+              size="14px"
+            />
+            <div className="row mt-5">
+              <div className="col-5">
+                <AtomButton
+                  background="#0086CF"
+                  value="Browse All Products"
+                  color="#FFF"
+                  border="none"
+                  padding="10px 20px"
+                  radius="10px"
+                />
+              </div>
+              <div className="col">
+              <AtomButton
+                background="#F0F0F0"
+                value="Follow This Board"
+                border="none"
+                padding="10px 20px"
+                radius="10px"
+              />
+              </div>
+            </div>
+          </div>
+          <div className="col">
+          <img src="/beranda/img_beranda.svg" />
+          </div>
+        </div>
+      </center>
+    </>
+  );
+}
