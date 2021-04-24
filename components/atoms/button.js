7ad653b9914d75ba410675592f292d6b3bc14705
armando-sys <patrickarmando87@gmail.com>
2021-04-24 13:10:34 +0700

    Update button.js

--- components/atoms/button.js
+++ components/atoms/button.js
@@ -1,14 +1,15 @@
-export default function button({ value, background, color, padding, border }) {
-    return (
-      <button
-        style={{
-          background: background,
-          color: color,
-          padding: padding,
-          border: border,
-        }}
-      >
-        {value}
-      </button>
-    );
-  }
+export default function button({ value, background, color, padding, border, radius }) {
+  return (
+    <button
+      style={{
+        background: background,
+        color: color,
+        padding: padding,
+        border: border,
+        borderRadius:radius
+      }}
+    >
+      {value}
+    </button>
+  );
+}
